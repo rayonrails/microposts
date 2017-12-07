4dad27ab3d000ffa2c2f0a23ffbb48f53c31eb43
Brad Traversy <me@bradtraversy.com>
2017-12-07 15:51:04 -0500

    remove sample code

--- src/app.js
+++ src/app.js
@@ -1,21 +1,2 @@
-// Import using destructuring
-import { person, sayHello } from './lib';
-alert(`Hello ${person.name}`);
-console.log(sayHello('Brad'));
-
-// Import all as object
-// import * as lib from './lib';
-// alert(`Hello ${lib.person.name}`);
-// console.log(lib.sayHello('Brad'));
-
-// Import default value
-// import name from './lib';
-// alert(name);
-
-async function getPosts(){
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await response.json();
-  return data;
-}
-
-getPosts().then(posts => console.log(posts));
+const greeting = 'Hello World';
+console.log(greeting);
